f220a019244a2eb8780e45ebe95e32da16b22915
Bhushan_bhatta <bhushanbhatta777@gmail.com>
2026-04-08 00:55:50 +0545

    query params and api ednpoint in flask

--- flask_revision/new_app/main.py
+++ flask_revision/new_app/main.py
@@ -1,10 +1,12 @@
 
-from flask import Flask , render_template, request
+from flask import Flask , render_template, request, flash,jsonify
 
 app= Flask(__name__) 
 
-@app.route("/")
+app.secret_key = "dfghjklas647298eudghfd"
+@app.route("/home")
 def home_page() :
+    flash("hello welcome")
     name  = "jack" 
 
     numbers  =   [ 1,2,4,56,123]
@@ -12,5 +14,41 @@ def home_page() :
 
 @app.route("/about")
 def about_page(): 
-    return render_template("about.html") 
+    flash("welcome to about page!")
+    return render_template("about.html")  
+
+
+
+## query param  
+@app.route("/")
+def hello_world () : 
+    name = request.args.get("name") 
+    print(name)
+    return render_template("index2.html", name=name)
+
+
+
+@app.route("/apitest") 
+def api_test() : 
+    data = {"output" : 45, "Accuracy" : 90.12}
+
+    return jsonify(data), 200
+
+
+@app.route('/api/predict', methods=['GET'])
+def predict():
+    value = request.args.get('value')
+    
+    if value is None:
+        return jsonify({'error': 'Missing input'}), 400
+ 
+    # Simulated prediction (replace with actual model call)
+    result = int(value) * 2
+ 
+    return jsonify({
+        'input': value,
+        'prediction': result
+    }) 
+##test with : /api/predict?value=7
+
 app.run(debug=True)
